067ff16e78db35b86a546c9fd24ef1a1c110ebd7
pjadhav5510 <112354938+pjadhav5510@users.noreply.github.com>
2024-02-28 15:37:53 -0800

    Time: 8094 ms (8.02%) | Memory: 16.6 MB (67.03%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        
+        t,ans="",""
+        size=0
+        for i in range(len(s)):
+            for j in range(i,len(s)):
+                t+=s[j]
+                if t==t[::-1] and len(t)>size:
+                    ans=t
+                    size=len(t)
+            t=""
+        return ans
