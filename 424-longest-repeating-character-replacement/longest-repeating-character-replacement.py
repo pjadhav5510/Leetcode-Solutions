f589bbbb79c64985803876bb317ae706755bbeac
pjadhav5510 <112354938+pjadhav5510@users.noreply.github.com>
2024-02-28 16:17:08 -0800

    Time: 462 ms (5.03%) | Memory: 16.6 MB (67.08%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,14 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        
+        maxlen, largestCount = 0, 0
+        arr = collections.Counter()
+        for idx in range(len(s)):
+            print(arr)
+            arr[s[idx]] += 1
+            largestCount = max(largestCount, arr[s[idx]])
+            if maxlen - largestCount >= k:
+                arr[s[idx - maxlen]] -= 1
+            else:
+                maxlen += 1
+        return maxlen
